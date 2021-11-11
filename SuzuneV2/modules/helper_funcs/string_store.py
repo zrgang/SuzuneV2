ee2ed0b4daa6970e6d8b12d6482f039799718aa6
desinobita <84653553+desinobita@users.noreply.github.com>
2021-11-11 20:13:40 +0530

    Add files via upload

---
+++ SuzuneV2/modules/helper_funcs/string_store.py
@@ -0,0 +1,7 @@
+DIE = [
+    "Hey Please Stop 🥺 Just Think About Your Mom, She'll Just Die There If She Saw You In Died. Just Enjoy Your Life And If Want Advice You Can Contact A Counselor :- 9152987821",
+]
+
+WARNING = [
+    "🚨WARNING🚨 STOP FALLING IN LOVE! PHADAI KARO OR ENJOY KARO BHAIYO KE SATH YE RANDI RONA MAI KUCH NAHI RAKHA HAI. MERA KAAM BATANA BAKHI TERI MARJI KAL KATEGA TO DM KARNA😉 MAI KAHA JAUNGI ✌",
+]
