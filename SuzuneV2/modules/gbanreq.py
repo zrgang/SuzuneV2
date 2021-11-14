c01b8cae62c863378dd930f8a32c392d21d1909f
desinobita <84653553+desinobita@users.noreply.github.com>
2021-11-14 13:16:45 +0530

    Add files via upload

---
+++ SuzuneV2/modules/gbanreq.py
@@ -0,0 +1,114 @@
+@run_async
+@support_plus
+def gbanreq(update: Update, context: CallbackContext):
+    bot, args = context.bot, context.args
+    message = update.effective_message
+    user = update.effective_user
+    chat = update.effective_chat
+    log_message = ""
+
+    user_id, reason = extract_user_and_text(message, args)
+
+    if not user_id:
+        message.reply_text(
+            "You don't seem to be referring to a user or the ID specified is incorrect.."
+        )
+        return
+
+    if int(user_id) in DEV_USERS:
+        message.reply_text(
+            "That user is part of the Association\nI can't act against our own."
+        )
+        return
+
+    if int(user_id) in DRAGONS:
+        message.reply_text(
+            "I spy, with my little eye... a disaster! Why are you guys turning on each other?"
+        )
+        return
+
+    if int(user_id) in DEMONS:
+        message.reply_text(
+            "OOOH someone's trying to gban a Demon Disaster! *grabs popcorn*"
+        )
+        return
+
+    if int(user_id) in TIGERS:
+        message.reply_text("That's a Tiger! They cannot be banned!")
+        return
+
+    if int(user_id) in WOLVES:
+        message.reply_text("That's a Wolf! They cannot be banned!")
+        return
+
+    if user_id == bot.id:
+        message.reply_text("You uhh...want me to punch myself?")
+        return
+
+    if user_id in [777000, 1087968824]:
+        message.reply_text("Fool! You can't attack Telegram's native tech!")
+        return
+
+    try:
+        user_chat = bot.get_chat(user_id)
+    except BadRequest as excp:
+        if excp.message == "User not found":
+            message.reply_text("I can't seem to find this user.")
+            return ""
+        else:
+            return
+
+    if user_chat.type != "private":
+        message.reply_text("That's not a user!")
+        return
+
+    if sql.is_user_gbanned(user_id):
+
+        if not reason:
+            message.reply_text(
+                "This user is already gbanned; I'd change the reason, but you haven't given me one..."
+            )
+            return
+
+        old_reason = sql.update_gban_reason(
+            user_id, user_chat.username or user_chat.first_name, reason
+        )
+        if old_reason:
+            message.reply_text(
+                "This user is already gbanned, for the following reason:\n"
+                "<code>{}</code>\n"
+                "I've gone and updated it with your new reason!".format(
+                    html.escape(old_reason)
+                ),
+                parse_mode=ParseMode.HTML,
+            )
+
+        else:
+            message.reply_text(
+                "This user is already gbanned, but had no reason set; I've gone and updated it!"
+            )
+
+        return
+
+    message.reply_text("On it!")
+
+    start_time = time.time()
+    datetime_fmt = "%Y-%m-%dT%H:%M"
+    current_time = datetime.utcnow().strftime(datetime_fmt)
+
+    if chat.type != "private":
+        chat_origin = "<b>{} ({})</b>\n".format(html.escape(chat.title), chat.id)
+    else:
+        chat_origin = "<b>{}</b>\n".format(chat.id)
+
+    log_message = (
+        f"#GBANNED\n"
+        f"<b>Originated from:</b> <code>{chat_origin}</code>\n"
+        f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
+        f"<b>Banned User:</b> {mention_html(user_chat.id, user_chat.first_name)}\n"
+        f"<b>Banned User ID:</b> <code>{user_chat.id}</code>\n"
+        f"<b>Event Stamp:</b> <code>{current_time}</code>"
+    )
+    
+    
+GBANREQ_HANDLER = CommandHandler("gban", gban)
