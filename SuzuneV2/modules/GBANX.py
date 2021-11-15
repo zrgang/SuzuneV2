93295239ce41ae4b6dfdc237478bafed6d3d90af
desinobita <84653553+desinobita@users.noreply.github.com>
2021-11-15 08:09:04 +0530

    Add files via upload

---
+++ SuzuneV2/modules/GBANX.py
@@ -0,0 +1,212 @@
+import html
+import time
+from datetime import datetime
+from io import BytesIO
+
+from telegram import ParseMode, Update
+from telegram.error import BadRequest, TelegramError, Unauthorized
+from telegram.ext import (
+    CallbackContext,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+    run_async,
+)
+from telegram.utils.helpers import mention_html
+
+import SuzuneV2.modules.sql.global_bans_sql as sql
+from SuzuneV2.modules.sql.users_sql import get_user_com_chats
+from SuzuneV2 import (
+    DEV_USERS,
+    EVENT_LOGS,
+    OWNER_ID,
+    STRICT_GBAN,
+    DRAGONS,
+    SUPPORT_CHAT,
+    SPAMWATCH_SUPPORT_CHAT,
+    DEMONS,
+    TIGERS,
+    WOLVES,
+    sw,
+    dispatcher,
+)
+from SuzuneV2.modules.helper_funcs.chat_status import (
+    is_user_admin,
+    support_plus,
+    user_admin,
+)
+from SuzuneV2.modules.helper_funcs.extraction import (
+    extract_user,
+    extract_user_and_text,
+)
+from SuzuneV2.modules.helper_funcs.misc import send_to_list
+
+GBAN_ENFORCE_GROUP = 6
+
+GBAN_ERRORS = {
+    "User is an administrator of the chat",
+    "Chat not found",
+    "Not enough rights to restrict/unrestrict chat member",
+    "User_not_participant",
+    "Peer_id_invalid",
+    "Group chat was deactivated",
+    "Need to be inviter of a user to kick it from a basic group",
+    "Chat_admin_required",
+    "Only the creator of a basic group can kick group administrators",
+    "Channel_private",
+    "Not in the chat",
+    "Can't remove chat owner",
+}
+
+UNGBAN_ERRORS = {
+    "User is an administrator of the chat",
+    "Chat not found",
+    "Not enough rights to restrict/unrestrict chat member",
+    "User_not_participant",
+    "Method is available for supergroup and channel chats only",
+    "Not in the chat",
+    "Channel_private",
+    "Chat_admin_required",
+    "Peer_id_invalid",
+    "User not found",
+}
+
+@run_async
+@support_plus
+def gbanop(update: Update, context: CallbackContext):
+    bot, args = context.bot, context.args
+    message = update.effective_message
+    user = update.effective_user
+    chat = update.effective_chat
+    log_message = ""
+
+    user_id, reason = extract_user_and_text(message, args)
+
+    if not user_id:
+        message.reply_text(
+            "You don't seem to be referring to a user or the ID specified is incorrect.."
+        )
+        return
+
+    if int(user_id) in DEV_USERS:
+        message.reply_text(
+            "That user is part of the Association\nI can't act against our own."
+        )
+        return
+
+    if int(user_id) in DRAGONS:
+        message.reply_text(
+            "I spy, with my little eye... a disaster! Why are you guys turning on each other?"
+        )
+        return
+
+    if int(user_id) in DEMONS:
+        message.reply_text(
+            "OOOH someone's trying to gban a Demon Disaster! *grabs popcorn*"
+        )
+        return
+
+    if int(user_id) in TIGERS:
+        message.reply_text("That's a Tiger! They cannot be banned!")
+        return
+
+    if int(user_id) in WOLVES:
+        message.reply_text("That's a Wolf! They cannot be banned!")
+        return
+
+    if user_id == bot.id:
+        message.reply_text("You uhh...want me to punch myself?")
+        return
+
+    if user_id in [777000, 1087968824]:
+        message.reply_text("Fool! You can't attack Telegram's native tech!")
+        return
+
+    try:
+        user_chat = bot.get_chat(user_id)
+    except BadRequest as excp:
+        if excp.message == "User not found":
+            message.reply_text("I can't seem to find this user.")
+            return ""
+        else:
+            return
+
+    if user_chat.type != "private":
+        message.reply_text("That's not a user!")
+        return
+
+    if sql.is_user_gbanned(user_id):
+
+        if not reason:
+            message.reply_text(
+                "This user is already gbanned; I'd change the reason, but you haven't given me one..."
+            )
+            return
+
+        old_reason = sql.update_gban_reason(
+            user_id, user_chat.username or user_chat.first_name, reason
+        )
+        if old_reason:
+            message.reply_text(
+                "This user is already gbanned, for the following reason:\n"
+                "<code>{}</code>\n"
+                "I've gone and updated it with your new reason!".format(
+                    html.escape(old_reason)
+                ),
+                parse_mode=ParseMode.HTML,
+            )
+
+        else:
+            message.reply_text(
+                "This user is already gbanned, but had no reason set; I've gone and updated it!"
+            )
+
+        return
+
+    message.reply_text("On it!")
+
+    start_time = time.time()
+    datetime_fmt = "%Y-%m-%dT%H:%M"
+    current_time = datetime.utcnow().strftime(datetime_fmt)
+
+    if chat.type != "private":
+        chat_origin = "<b>{} ({})</b>\n".format(html.escape(chat.title), chat.id)
+    else:
+        chat_origin = "<b>{}</b>\n".format(chat.id)
+
+    log_message = (
+        f"🚨GBAN NOTIFICATION🚨\n"
+        f"<b>⭐From:</b> <code>{chat_origin}</code>\n"
+        f"<b>⭐Admin:</b> {mention_html(user.id, user.first_name)}\n"
+        f"<b>⚠️Banned User:</b> {mention_html(user_chat.id, user_chat.first_name)}\n"
+        f"<b>⚠️Banned User ID:</b> <code>{user_chat.id}</code>\n"
+        f"<b>✅Event Stamp:</b> <code>{current_time}</code>"
+    )
+
+    if reason:
+        if chat.type == chat.SUPERGROUP and chat.username:
+            log_message += f'\n<b>Reason:</b> <a href="https://telegram.me/{chat.username}/{message.message_id}">{reason}</a>'
+        else:
+            log_message += f"\n<b>Reason:</b> <code>{reason}</code>"
+
+    if EVENT_LOGS:
+        try:
+            log = bot.send_message(EVENT_LOGS, log_message, parse_mode=ParseMode.HTML)
+        except BadRequest as excp:
+            log = bot.send_message(
+                EVENT_LOGS,
+                log_message
+                + "\n\nFormatting has been disabled due to an unexpected error.",
+            )
+
+GBAN_HANDLER = CommandHandler("gban", gbanop)
+
+GBAN_ENFORCER = MessageHandler(Filters.all & Filters.group, enforce_gban)
+
+dispatcher.add_handler(GBAN_HANDLER)
+
+__handlers__ = [GBAN_HANDLER]
+
+if STRICT_GBAN:  # enforce GBANS if this is set
+    dispatcher.add_handler(GBAN_ENFORCER, GBAN_ENFORCE_GROUP)
+    __handlers__.append((GBAN_ENFORCER, GBAN_ENFORCE_GROUP))
