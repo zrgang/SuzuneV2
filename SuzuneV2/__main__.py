bd3f4c278d4ee3f44d53903df2dc1eeaae3c403b
desinobita <84653553+desinobita@users.noreply.github.com>
2021-11-16 15:42:39 +0530

    Update __main__.py

--- SuzuneV2/__main__.py
+++ SuzuneV2/__main__.py
@@ -99,8 +99,21 @@ buttons = [
 
 
 HELP_STRINGS = """
-Help Settings As Follows
-Click on the Buttons To Know More About That Commands 😉"""
+Hey! My name is Suzune Horikita. I am a group management bot, here to help you get around and keep the order in your groups!
+
+I have lots of handy features, such as flood control, a warning system, a note keeping system, and even predetermined replies on certain keywords.
+
+Helpful commands:
+
+- /start: Starts me! You've probably already used this.
+
+- /help: Sends this message; I'll tell you more about myself!
+
+- /donate: Gives you info on how to support me and my creator.
+
+If you have any bugs or questions on how to use me, Contact My Owner At PM @xxxNOBITAxxx, or head to @Suzune_Support 
+
+ All commands can be used with the following: / !"""
 
 
 
